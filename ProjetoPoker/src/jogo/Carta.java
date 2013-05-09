329884488109c614f2fd713ace05561ec7f5f2dc
Eduardo Vergne Dias <eduardovdias@hotmail.com>
2013-05-09 18:48:10 -0300

    Limpei algumas coisas hehe

--- ProjetoPoker/src/jogo/Carta.java
+++ ProjetoPoker/src/jogo/Carta.java
@@ -1,40 +1,24 @@
 package jogo;
 
 public class Carta {
-	String face,suit;
-	public String endereço; 
+	
+	String s_valor, s_naipe;
 	int valor, naipe; 
 
-	int valortemp = 0;
-	String naipetemp = "";
-
-	public Carta( String cardFace, String cardSuit, int cardValor, int cardNaipe) //Construtor da classe Carta
+	
+	public Carta( String s_valor, String s_naipe, int cardValor, int cardNaipe) //Construtor da classe Carta
 	{
-		face= cardFace;
-		suit= cardSuit;
+		
+		this.s_valor= s_valor;
+		this.s_naipe= s_naipe;
 		valor = cardValor;
 		naipe = cardNaipe;
 		
-		
-		
-		if (valor< 13){
-			valortemp = valor +1; 
-		} else{
-			valortemp = valor - 12; 
-		} //Este laço atribui o valor das cartas, onde o ás é 1, 2 é 2, ... , Rei é 12
-			
-	   	switch(naipe){
-	   	
-	   	case 0: naipetemp = "c"; break;
-	   	case 1: naipetemp = "d"; break;
-	   	case 2: naipetemp = "h"; break;
-	   	case 3: naipetemp = "s"; break;
-	    	
-	   	} //Este laço atribui um valor String para os naipes, onde c é cloves(paus), d é diamonds(ouros), h é hearts(copas) e s é spades(espadas)
-	   	
-	   	endereço = "img/"+valortemp+naipetemp+".png"; //É atribuido o endereço da variável endereço assim como deve ser formatado para ser acessado no package img
-		
-		
+
+	}
+	
+	public String toString(){
+		return s_valor + " de " + s_naipe ; 
 	}
 		
 }
---
+++ ProjetoPoker/src/jogo/Jogador.java
@@ -0,0 +1,18 @@
+package jogo;
+
+public class Jogador {
+	
+	String nome;
+	Carta mao_01;
+	Carta mao_02;
+	
+	public Jogador(String nome, Carta mao_01, Carta mao_02){
+		
+		this.nome = nome;
+		this.mao_01 = mao_01;
+		this.mao_02 = mao_02;
+					
+	}
+	
+	
+}
